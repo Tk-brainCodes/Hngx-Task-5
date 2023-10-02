c4d6c02cc29049868d5016d917829f69f802b9b7
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2023-10-02 10:06:46 +0100

    feat: chrome extension

---
+++ extension/src/App.jsx
@@ -0,0 +1,64 @@
+import { useContext, useState } from 'react'
+import { Context } from './Context'
+
+import ScreenRecording from './components/ScreenRecorder'
+import Widget from './components/Widget'
+
+function App () {
+  const { status } = useContext(Context)
+  const [closePopup, setClosePopup] = useState(true)
+
+  const handleClosePopup = () => {
+    setClosePopup(!closePopup)
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    // Adjust the vertical position by changing the window.innerHeight value
+    const popup = document.getElementById('popup-container')
+    const verticalOffset = 100 // Adjust this value as needed
+
+    console.log(window.innerHeight - verticalOffset, popup)
+
+    // popup.style.height = `${window.innerHeight - verticalOffset}px`;
+  })
+
+  return (
+    <div>
+      {status !== 'recording' ? (
+        <>
+          {closePopup && (
+            <div className='app'>
+              <dialog
+                className='dialog'
+                open={status === 'acquiring_media'}
+                id='dialog'
+              >
+                Waiting for screen recording authorization
+              </dialog>
+
+              <div
+                style={{
+                  width: `${status === 'acquiring_media' ? '700px' : '300px'}`,
+                  height: `${status === 'acquiring_media' ? '500px' : '439px'}`
+                }}
+              >
+                <ScreenRecording handleClosePopup={handleClosePopup} />
+              </div>
+            </div>
+          )}
+        </>
+      ) : (
+        <div className='widget' id=''>
+          <Widget />
+        </div>
+      )}
+
+      <div id='video'></div>
+
+      {/**Video file to be displayed after recording */}
+      {/* <Preview /> */}
+    </div>
+  )
+}
+
+export default App
